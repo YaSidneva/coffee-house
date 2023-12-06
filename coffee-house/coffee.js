00096dcb0f06e16f9f63ca82dcee477631539c9d
Yana Sidneva <yan-zublenk2008@narod.ru>
2023-12-06 17:50:17 +0100

    feat: implemente burger-menu with js

---
+++ coffee-house/coffee.js
@@ -0,0 +1,14 @@
+function closeMenuOnClick() {
+    const burgerLinksArr = document.getElementsByClassName('burger-link');
+    const checkbox = document.getElementById('is-check');
+
+    for (let i = 0; i <= burgerLinksArr.length - 1; i++) {
+        let burgerLink = burgerLinksArr[i];
+        console.log(burgerLink);
+        burgerLink.addEventListener("click", () => {
+            checkbox.checked = false;
+        });
+    }
+}
+
+closeMenuOnClick();
